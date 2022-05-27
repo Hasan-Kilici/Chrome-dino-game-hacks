b916cd40a70e74a263b87a7749a91d205bb17779
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-05-27 08:59:15 +0300

    Create deleteEnemys.js

---
+++ deleteEnemys.js
@@ -0,0 +1,32 @@
+Obstacle.types[0] = {
+collisionBoxes: [CollisionBox, CollisionBox, CollisionBox],
+height: 0,
+minGap: 0,
+minSpeed: 0,
+multipleSpeed: 4,
+type: "CACTUS_SMALL",
+width: 0,
+yPos: 0
+}
+Obstacle.types[1] = {
+collisionBoxes: [CollisionBox, CollisionBox, CollisionBox],
+height: 0,
+minGap: 0,
+minSpeed: 0,
+multipleSpeed: 7,
+type: "CACTUS_LARGE",
+width: 0,
+yPos: 0
+}
+Obstacle.types[2] = {
+collisionBoxes:[CollisionBox, CollisionBox, CollisionBox, CollisionBox, CollisionBox],
+frameRate: 166.66666666666666,
+height: 0,
+minGap: 0,
+minSpeed: 8.5,
+multipleSpeed: 999,
+numFrames: 2,
+speedOffset: 0.8,
+type: "PTERODACTYL",
+width: 0
+}

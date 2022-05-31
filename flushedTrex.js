710c6614f2d7f46e92c6c528417e8b5fa1b5e194
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-05-31 10:35:21 +0300

    Create flushedTrex.js

---
+++ flushedTrex.js
@@ -0,0 +1,16 @@
+Trex.animFrames.RUNNING = {
+frames: [220],
+msPerFrame: 16.666666666666668,
+}
+Trex.animFrames.JUMPING = {
+frames: [220],
+msPerFrame: 16.666666666666668,
+}
+Trex.animFrames.WAITING = {
+frames: [220],
+msPerFrame: 16.666666666666668,
+}
+Trex.animFrames.DUCKING = {
+frames: [220],
+msPerFrame: 16.666666666666668,
+}

5696645f481a9e770e552d8d88b49d096f8de512
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-06-01 15:01:56 +0300

    Create batmanMode.js

---
+++ batmanMode.js
@@ -0,0 +1,6 @@
+Runner.imageSprite.src = "https://chromedino.com/assets/batman1x.png";
+Trex.animFrames.RUNNING = {
+frames: [0,0],
+msPerFrame: 83.33333333333333
+}
+document.body.style.backgroundColor = "#333";
